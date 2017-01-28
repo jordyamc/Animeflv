4febf1bc1f7a21dde1e4dd91162ba4d646d9bf50
jordyamc <jordyamc@hotmail.com>
2017-01-28 09:50:42 -0600

    web

---
+++ docs/cloudbypass/cloud_bypass.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+from flask import request
+import cfscrape
+
+app = Flask(__name__)
+
+# When localhost access through 'http://127.0.0.1:5000/api/v1/content?url=http://animeflv.net/ver/one-piece-1.html'
+@app.route('/v1/content', methods=('get', 'post'))
+def get_tasks():
+    page='null';
+    try:
+        url = request.args.get('url')
+        type = request.args.get('type')
+
+        if type == 'html':
+            scraper = cfscrape.create_scraper()
+            page = scraper.get(url).content
+            return page
+
+        if type == 'cookies':
+            scraper = cfscrape.create_scraper()
+            tokens, user_agent = cfscrape.get_tokens(url)
+            response = {
+                'url': url,
+                'user_agent': user_agent,
+                'tokens_cookies': tokens
+            }
+            return jsonify(response)
+    except:
+        return page
+
+if __name__ == "__main__":
+    app.run()
